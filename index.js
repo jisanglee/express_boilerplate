256f6b8fefc4e0983e20fa36f3130556c3457a1c
jisanglee <newspatoy1@gmail.com>
2021-06-04 14:50:45 +0900

    using bcrypt hash

--- index.js
+++ index.js
@@ -7,19 +7,17 @@ const bodyParser = require('body-parser');
 const { User } = require('./models/User');
 
 //client에서 bodyParser를 통해 데이터 가져올수있게
-//application/x-www-form-urlencoded
+//application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//application/json
-app.use(bodyParser.json())
+//application/json  //postman에서 json 타입으로 전송하는게 중요.
+app.use(bodyParser.json());
 
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => {
-    console.log('MongoDB connected...');
-}).catch(err => console.log(err));
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello World!1111')
@@ -28,14 +26,14 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
     //회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 database에 넣음
     const user = User(req.body);
-    //model안에 세이브
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err }) //실패시
-        return res.status(200).json({
-            //성공시
-            success: true
-        })
+    
+    //model안에 세이브 save하기전에 bcrypt로 비번을 암호화
+    user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
     })
+  })
     
 });
 
--- models/User.js
+++ models/User.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //salt가몇글자인지? 10자리.
+
+
 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        maxlength:50,
+        maxlength:50
     },
     email: {
         type: String,
         trim: true,
-        unique:1,
+        unique:1
     },
     password: {
         type: String,
@@ -31,7 +35,27 @@ const userSchema = mongoose.Schema({
     }
 })
 
-//스키마 model로 감싸기
-const User = mongoose.model('User', userSchema);
+//user save하기전에 이걸 진행하고 그다음에 저장을함.
+userSchema.pre('save', function (next) {
+    var user = this;
 
-module.exports = { User };
+    //password가 변할때만 암호화 해준다(다른것이 바뀔때 암호화를 다시하면 안되므로)
+    if (user.isModified('password')) {
+        
+        //비밀번호를 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB. hash가 암호화된 비번.
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }
+})
+
+//스키마 model로 감싸기
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
